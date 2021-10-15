6fa3c4a1f9d3a8b599a74d58694e58f44e0d9c04
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-10-15 20:21:07 +0200

    adding RandomForest

--- my_lib/plotting.py
+++ my_lib/plotting.py
@@ -5,37 +5,39 @@ import matplotlib.pyplot as plt
 COLORS = {'train': 'b', 'test': 'r'}
 
 
-def plot_validation_curve(train_scores, test_scores, train_sizes, expected_score=None, ax=None):
+def plot_validation_curve(train_scores, test_scores, train_sizes, expected_score=None, ax=None, stat_error=True):
 
     ax = _plot_generic_curve(
         train_scores=train_scores,
         test_scores=test_scores,
         x_pars=train_sizes,
         expected_score=expected_score,
-        ax=ax
+        ax=ax,
+        stat_error=stat_error
     )
 
     ax.set_ylabel('score')
     ax.set_xlabel('training sample size')
 
 
-def plot_learning_curve(train_scores, test_scores, param_range, expected_score=None, ax=None):
+def plot_learning_curve(train_scores, test_scores, param_range, expected_score=None, ax=None, stat_error=True):
 
     ax = _plot_generic_curve(
         train_scores=train_scores,
         test_scores=test_scores,
         x_pars=param_range,
         expected_score=expected_score,
-        ax=None
+        ax=None,
+        stat_error=stat_error
     )
 
     ax.set_ylabel('score')
     ax.set_xlabel('parameter value')
 
 
-def _plot_generic_curve(train_scores, test_scores, x_pars, expected_score=None, ax=None):
+def _plot_generic_curve(train_scores, test_scores, x_pars, expected_score=None, ax=None, stat_error=True):
     train_score_mean, train_score_error, test_score_mean, test_score_error = \
-        get_score_means_and_errors(train_scores, test_scores)
+        get_score_means_and_errors(train_scores, test_scores, stat_error=stat_error)
 
     if ax is None:
         ax = plt.gca()
@@ -85,14 +87,19 @@ def _plot_generic_curve(train_scores, test_scores, x_pars, expected_score=None, 
     return ax
 
 
-def get_score_means_and_errors(train_scores, test_scores):
+def get_score_means_and_errors(train_scores, test_scores, stat_error=True):
     train_score_mean = np.mean(train_scores, axis=1)
-    train_score_error = np.std(train_scores, axis=1) / np.sqrt(len(train_scores[0]))
-
     test_score_mean = np.mean(test_scores, axis=1)
-    test_score_error = np.std(test_scores, axis=1) / np.sqrt(len(test_scores[0]))
+
+    if stat_error:
+        train_score_error = np.std(train_scores, axis=1) / np.sqrt(len(train_scores[0]))
+        test_score_error = np.std(test_scores, axis=1) / np.sqrt(len(test_scores[0]))
+    else:
+        train_score_error = np.std(train_scores, axis=1)
+        test_score_error = np.std(test_scores, axis=1)
 
     return train_score_mean, train_score_error, test_score_mean, test_score_error
+
 
 def plot_probability_distributions(clf, X, y):
     prob = clf.predict_proba(X)
